f127f1d174092f5e86a96feec2904ecfe7b7b9c6
Debora <deboradiorato7@gmail.com>
2024-10-19 21:27:30 -0300

    produtos cadastrados, aula finalizada

--- codigo.py
+++ codigo.py
@@ -28,3 +28,42 @@ pyautogui.click(x=798, y=275)
 # variavel tabela recebe o csv (ler um arquivo csv)
 tabela = pd.read_csv("produtos.csv")
 print(tabela)
+
+for linha in tabela.index:
+
+    codigo = tabela.loc[linha, "codigo"]  # pegar o valor da linha e coluna
+    # esqueleto do cadastro de produtos
+    # str para escrever o codigo em formato de texto
+    pyautogui.write(str(codigo))
+    pyautogui.press("tab")
+
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(str(marca))
+    pyautogui.press("tab")
+
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(str(tipo))
+    pyautogui.press("tab")
+
+    categoria = tabela.loc[linha, "categoria"]
+    pyautogui.write(str(categoria))
+    pyautogui.press("tab")
+
+    preco = tabela.loc[linha, "preco_unitario"]
+    pyautogui.write(str(preco))
+    pyautogui.press("tab")
+
+    custo = tabela.loc[linha, "custo"]
+    pyautogui.write(str(custo))
+    pyautogui.press("tab")
+
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):  # se nao tiver uma obs, nao vai escrever
+        pyautogui.write(str(obs))
+    pyautogui.press("tab")
+
+    pyautogui.press("enter")
+
+    pyautogui.scroll(5000)
+
+    pyautogui.click(x=798, y=275)
